b62a42c4361017aa7c4e3ff5c49c274cdc608fe8
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-24 17:21:50 -0500

    added build_user fn

--- the_rust_programming_language/structs_5_1/src/main.rs
+++ the_rust_programming_language/structs_5_1/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123")
+    );
 
     user1.email = String::from("anotheremail@example.com");
 }
@@ -14,4 +12,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
 }
