215052ad3a25b12a4886b8bc691f96ac2718177f
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-24 16:48:03 -0500

    return first word slice

--- the_rust_programming_language/ownership_4_1/src/main.rs
+++ the_rust_programming_language/ownership_4_1/src/main.rs
@@ -1,14 +1,15 @@
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
+
 
 fn main() {
     println!("{}", first_word(&String::from("Some words here")));
