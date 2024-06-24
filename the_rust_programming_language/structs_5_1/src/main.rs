ae71ec4859d2b9f48bad7fc71a72fe565653d4b2
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-24 17:31:19 -0500

    updated note

--- the_rust_programming_language/structs_5_1/src/main.rs
+++ the_rust_programming_language/structs_5_1/src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     user1.email = String::from("anotheremail@example.com");
 
     // add a user2 with same data as user1 except for email using struct update syntax
+    // note that user1 is no longer valid since the username field was moved to user2
+    // specifying a username field would not invalidate user1 because active and sign_in_count
+    // implement the Copy trait, since they are scalar types
     let user2 = User {
         email: String::from("another@example.com"),
         ..user1
