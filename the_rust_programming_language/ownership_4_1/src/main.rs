da25d75699fbe72a120f54e173adae8556364273
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-24 15:06:40 -0500

    dangling ref - errors

--- the_rust_programming_language/ownership_4_1/src/main.rs
+++ the_rust_programming_language/ownership_4_1/src/main.rs
@@ -1,15 +1,9 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{r1} and {r2}");
-    // variables r1 and r2 will not be used after this point
-
-    let r3 = &mut s; // no problem
-    println!("{r3}");
+    let reference_to_nothing = dangle();
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
 }
