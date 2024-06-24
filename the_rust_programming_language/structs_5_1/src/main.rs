ec3cb35b7986b2583f2ce3e84c07c39f60feea51
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-24 17:34:19 -0500

    tuple struct - added notes

--- the_rust_programming_language/structs_5_1/src/main.rs
+++ the_rust_programming_language/structs_5_1/src/main.rs
@@ -1,33 +1,11 @@
+// Tuple structs allow adding names to tuples. They have the added benefit of making the tuple struct's
+// purpose more clear. Tuple structs are useful when you want to give the whole tuple a name and make the
+// tuple be a different type from other tuples, and naming each field as a struct would be overkill.
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
-    let mut user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123")
-    );
-
-    user1.email = String::from("anotheremail@example.com");
-
-    // add a user2 with same data as user1 except for email using struct update syntax
-    // note that user1 is no longer valid since the username field was moved to user2
-    // specifying a username field would not invalidate user1 because active and sign_in_count
-    // implement the Copy trait, since they are scalar types
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-}
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
