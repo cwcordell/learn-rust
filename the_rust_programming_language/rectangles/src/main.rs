edf9450f6750dfe509e38305dfc62471e45e2449
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-25 16:33:52 -0500

    defining methods

--- the_rust_programming_language/rectangles/src/main.rs
+++ the_rust_programming_language/rectangles/src/main.rs
@@ -4,21 +4,20 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: (30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
-
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
