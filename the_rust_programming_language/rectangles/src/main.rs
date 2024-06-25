96190336b13805cd692ac873732157926846eed2
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-25 15:46:21 -0500

    working rect area app with dbg

--- the_rust_programming_language/rectangles/src/main.rs
+++ the_rust_programming_language/rectangles/src/main.rs
@@ -7,9 +7,18 @@ struct Rectangle {
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: (30 * scale),
         height: 50,
     };
 
     dbg!(&rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
