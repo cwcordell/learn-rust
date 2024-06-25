1b9e0fc4fae4542389400a469a626f36b1cb2253
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-25 15:04:46 -0500

    initial rect area fn

--- the_rust_programming_language/structs_5_1/src/main.rs
+++ the_rust_programming_language/structs_5_1/src/main.rs
@@ -1,6 +1,6 @@
 // Unit-Like Structs are structs without any fields.
 // They are useful in situations in which you need to implement a trait on some type but don't have any data to store in the type itself.
-// The behave similarly to unit types, an empty tuple, which are written as ().
+// The behave similarly to unit types empty tuple, which are written as ().
 
 struct AlwaysEqual;
 
