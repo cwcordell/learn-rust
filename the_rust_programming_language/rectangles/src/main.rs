2ca33e2432455eb18ed5e8aaba463445fb8c35e6
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-25 17:37:17 -0500

    formatting

--- the_rust_programming_language/rectangles/src/main.rs
+++ the_rust_programming_language/rectangles/src/main.rs
@@ -6,8 +6,12 @@ struct Rectangle {
 
 impl Rectangle {
     fn square(size: u32) -> Self {
-        Self { width: size, height: size }
+        Self {
+            width: size,
+            height: size,
+        }
     }
+    
     fn area(&self) -> u32 {
         self.width * self.height
     }
@@ -15,7 +19,7 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
-    
+
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -46,5 +50,4 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-
 }
